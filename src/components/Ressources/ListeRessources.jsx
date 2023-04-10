18a561688788f5d192036caefd86b7212675ef9f
Pierrick <moreaupierrick.mp@gmail.com>
2023-04-10 22:01:37 +0200

    Affichage un peu plus sympa des ressources

--- src/components/Ressources/ListeRessources.jsx
+++ src/components/Ressources/ListeRessources.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { useState } from "react";
 import PublicationService from "../../services/PublicationService";
+import "../../styles/AffichageRessources.scss";
 
 
-export default function ListeRessources(){
+export default function ListeRessources() {
 
-    const [publications, setPublications] = useState([]);
+  const [publications, setPublications] = useState([]);
 
-    React.useEffect(() => {
-      PublicationService.getAllPublications().then((publications)=>{
-        setPublications(publications)
-      })
-    }, []);
-  
-    if (!publications) return null;
-    return (
-        <div>
-                     <h1 className="center">Liste des ressources</h1>
-                    <ul>
-                        {publications.map((publication) => (
-                           <li key={publication.id}>
-                               {publication.titre}
-                           </li>
-                      ))}
-                   </ul>
-              </div>
-    );
-    }
+  React.useEffect(() => {
+    PublicationService.getAllPublications().then((publications) => {
+      setPublications(publications)
+    })
+  }, []);
+
+  if (!publications) return null;
+  return (
+    <div className="ressource-card ">
+      {publications.map((publication) => (
+        <div key={publication.id}>
+          <h2>{publication.titre}</h2>
+          <p>Mis en ligne le {publication.dateCreation} par {publication.idUtilisateur}</p>
+          <img src="publication.contenu" alt="publication.titre" />
+          <p>{publication.contenu}</p>
+          <p>{publication.idCategorie}</p>
+          <button click="RessourceUnique()">Voir plus</button>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 //     const [publications, setPublications] = useState<PublicationEntity[]>([]);
 

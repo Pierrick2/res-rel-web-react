08d276a477b0e4fe25629ef9f898ef374ecbd8e7
Pierrick <pierrick211@gmail.com>
2023-04-19 16:54:56 +0200

    toujours un problème d'affichage dans la page ressource à l'unité pas d'affichage des commentaires

--- src/components/Ressources/RessourceUnique.jsx
+++ src/components/Ressources/RessourceUnique.jsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import PublicationService from "../../services/PublicationService";
 import "../../styles/AffichageRessources.scss";
+import "../../styles/RessourceUnique.scss"
 import CommentaireService from "../../services/CommentaireService";
-import CommentaireRessource from "./CommentaireRessource";
 
 export default function RessourceUnique() {
   const [publication, setPublications] = useState([]);
+  const [commentaires, setCommentaires] = useState([]);
 
   const { id } = useParams();
 
-  React.useEffect(() => {
+  useEffect(() => {
     PublicationService.getPublication(id).then((publication) => {
       setPublications(publication);
+      CommentaireService.getCommentairesByRessourceId(id).then(
+        (commentaires) => {
+          setCommentaires(commentaires);
+        }
+      );
     });
   }, [id]);
 
@@ -25,12 +31,25 @@ export default function RessourceUnique() {
         {publication.idUtilisateur}
       </p>
       {publication.contenu && (
-        <img src={publication.contenu} alt="Contenu multimedia" />
+        <img src="https://picsum.photos/200/300" alt="Image de la publication" />
       )}
       <p>{publication.contenu}</p>
       <p>Catégorie {publication.idCategorie}</p>
-      <button>Commenter</button>
-      <CommentaireRessource />
+      <textarea
+          placeholder="Votre commentaire..."
+        ></textarea>
+      <button className="comment-btn">Commenter</button>
+      {commentaires.length > 0 && (
+        <div>
+          {commentaires.map((commentaire) => (
+            <div key={commentaire.id}>
+              <p>"{commentaire.contenu}"</p>
+              <p>Posté par {commentaire.idUtilisateur}</p>
+              <p>Le {commentaire.dateCreation}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/services/CommentaireService.js
+++ src/services/CommentaireService.js
@@ -1,19 +1,34 @@
-import { PublicationEntity } from "../ressources/models/PublicationEntity";
+
 import RestClient from "./RestClient";
 
 export default class CommentaireService {
-baseUrl = "commentaires";
+  baseUrl = "commentaires";
+  restClient;
 
-restClient;
-
-constructor() {
+  constructor() {
     this.restClient = new RestClient();
   }
   
   async getCommentairesByRessourceId(idRessource) {
-    const response = await this.restClient.get(
-      `${this.baseUrl}/${idRessource}`
-      );
-      return response.data;
-    }
+    const response = await this.restClient.get(`${this.baseUrl}/${idRessource}`);
+    return response.data;
+  }
+
+  async addCommentaire(commentaire) {
+    const response = await this.restClient.post(this.baseUrl, commentaire);
+    return response.data;
+  }
+
+  async updateCommentaire(commentaire) {
+    const response = await this.restClient.put(
+      `${this.baseUrl}/${commentaire.id}`,
+      commentaire
+    );
+    return response.data;
+  }
+
+  async deleteCommentaire(id) {
+    const response = await this.restClient.delete(`${this.baseUrl}/${id}`);
+    return response.data;
+  }
 }
